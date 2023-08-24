7a1a6099b9d20bb1fb7865758a79c761e0a5c135
hungcao291099 <giahung1135@gmail.com>
2023-08-24 07:01:48 +0700

    first commit

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,10 @@
+import Header from '../../components/Layout/DefaultLayout/Header';
+function Upload() {
+  return (
+    <div className="content">
+      <h2>Upload</h2>
+    </div>
+  );
+}
+
+export default Upload;
